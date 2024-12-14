390dd77f75b904b2289507acfb8ad4a3a5d13d0e
Jimmy <mgsa112233@gmail.com>
2024-12-14 15:36:48 +0200

    this have 3 file not in one file

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
-console.log('Happy developing ✨')
+function Length_Iterative() {
+    alert('Length_Iterative() called');
+}
+
+function Length_Recursive() {
+    alert('Length_Recursive() called');
+}
+
+function Swap() {
+    alert('Swap() called');
+}
+
+function Reverse() {
+    alert('Reverse() called');
+}
+
+function Merge() {
+    alert('Merge() called');
+}
+
+function Max() {
+    alert('Max() called');
+}
+
+function Min() {
+    alert('Min() called');
+}
+
+function Sum() {
+    alert('Sum() called');
+}
+
+function Aver() {
+    alert('Aver() called');
+}
+
+function Median() {
+    alert('Median() called');
+}
+
+function StrDev() {
+    alert('StrDev() called');
+}
+
+function Max_Second() {
+    alert('Max_Second() called');
+}
+
+function Min_Second() {
+    alert('Min_Second() called');
+}
+
+function Split() {
+    alert('Split() called');
+}
+
+function Insert() {
+    alert('Insert() called');
+}
+
+function Sorted() {
+    alert('Sorted() called');
+}
+
+function Even() {
+    alert('Even() called');
+}
+
+function Odd() {
+    alert('Odd() called');
+}
