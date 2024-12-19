5b422a149ebf9f7289dd40ac26f13032caae00d1
Jimmy <mgsa112233@gmail.com>
2024-12-19 10:50:16 +0200

    the all page we want

--- index.js
+++ index.js
@@ -1,71 +1,81 @@
 function Length_Iterative() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Length_Recursive() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Swap() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Reverse() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Merge() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Max() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Min() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Sum() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Aver() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Median() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function StrDev() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Max_Second() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Min_Second() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Split() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Insert() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Sorted() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Even() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
 
 function Odd() {
-    window.location.href = 'theTwoButton.html';
+    window.location.href = 'pages/theTwoButton.html';
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll('.button');
+
+    buttons.forEach((button, index) => {
+        setTimeout(() => {
+            button.classList.add('show'); // Add the show class to make it visible
+        }, index * 500); // Delay each button by 300ms multiplied by its index
+    });
+});
